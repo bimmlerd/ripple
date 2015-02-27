cf7f2c2e95a07cb6f09e8a7c872a9644296b03e6
David Bimmler <bimmlerd@student.ethz.ch>
2015-02-27 15:20:58 +0100

    messages work

--- client/current_channel.js
+++ client/current_channel.js
@@ -2,9 +2,23 @@ Template.currentChannel.events({
 	"click .new-message": function (event) {
     // This function is called when the new task form is submitted
     event.preventDefault();
-    console.log(event)
-    var message = event.target.text.value;
+    var input_element = document.getElementById('new_message_input');
+    var message = input_element.value;
+
+    Messages.insert({
+		"created": new Date(),
+		"author": "user",
+		"content": message,
+		"channelID": this.channelID
+    });
+// Message = {
+// 	"created": 0,
+// 	"author": "user",
+// 	"content": "This is le sample message",
+// 	"channelID": "kbsSiW49MMQtHWshR"
+// }
+
     // Clear form
-    event.target.text.value = "";	
+    input_element.value = "";
 	}
 });
