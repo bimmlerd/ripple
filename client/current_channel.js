b5e5f33984db196673bf09afe59756fffec396de
David Bimmler <bimmlerd@student.ethz.ch>
2015-02-27 09:55:23 +0100

    merge

--- client/current_channel.js
+++ client/current_channel.js
@@ -2,6 +2,7 @@ Template.currentChannel.events({
 	"click .new-message": function (event) {
     // This function is called when the new task form is submitted
     event.preventDefault();
+    console.log(event)
     var message = event.target.text.value;
     // Clear form
     event.target.text.value = "";	
